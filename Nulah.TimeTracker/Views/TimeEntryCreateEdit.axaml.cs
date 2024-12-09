b50781b925502874ddb6f5341546c6974b76e6cf
ZacMillionaire <scott.schultz@jotunga.com>
2024-12-09 17:40:05 +1000

    merge develop into main (#7)
    * Create DateView Base
    - add FlexingTimeSummary control
    
    * Update DateView to list time entries for selected date
    - update TimeEntrySmall to display duration
    - add Avalonia.Behaviours package to allow command binding to events
    - add default background colour to time entry properies
    
    * DateView update for selecting and list
    - update data to return a summarised view
    - add getting time entries for specific date
    - remove async from sqlite
    - update flexing time controls away from TimeEntryDto
    
    * Change summary to DateTime over DateOnly to simplify
    - add selected time summary
    - add first pass select matching summary on create
    
    * Update DateView
    - update to better respond to date outside of current displayed weel
    - calendar to pick weeks by selected dates
    - always show the entire week
    - refactor selected properties updates to single method call
    
    * Update FlexingTimeSummary to respect height
    
    * Make initial selected date consistent with calendar select
    
    * Implement create edit auto complete box partial functionality
    - update taskname auto complete box to search and display aggregate results
    - add transparent colour fallback if none given
    - add template to auto complete box to display results
    - implement time entry search aggregate functionality
    
    * Update task name autocomplete to select taskname and colour
    
    * Update date view to react to updates
    - refactor summaries to use SummarisedTimeEntryViewModel

--- Nulah.TimeTracker/Views/TimeEntryCreateEdit.axaml.cs
+++ Nulah.TimeTracker/Views/TimeEntryCreateEdit.axaml.cs
@@ -1,10 +1,7 @@
-﻿using System.Diagnostics;
-using System.Reactive.Disposables;
-using Avalonia;
+﻿using System.Reactive.Disposables;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using Nulah.TimeTracker.Domain.Models;
 using Nulah.TimeTracker.ViewModels;
 using ReactiveUI;
 using ReactiveUI.Validation.Extensions;
@@ -24,5 +21,17 @@ public partial class TimeEntryCreateEdit : ReactiveUserControl<TimeEntryCreateEd
 					v => v.StartEndValidationError.Text)
 				.DisposeWith(disposables);
 		});
+	}
+
+	private void AutoCompleteBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+	{
+		// TODO: reimplement this as not an auto complete box because it raises selection events once the asyncpopulator
+		// returns.
+		// It's dumb and I don't care to work around it
+		if (sender is AutoCompleteBox && e.AddedItems is [TimeEntrySearchAggregatedSuggestion selectedSuggestion])
+		{
+			ViewModel?.SetNameAndColourFromSelectedSuggestion(selectedSuggestion);
+			e.Handled = true;
+		}
 	}
 }
